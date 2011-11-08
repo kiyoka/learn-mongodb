5de1c100d243b8d72010c3525d57ecea5fcc51b7
Kiyoka Nishiyama <kiyoka@sumibi.org>
2011-11-09 06:42:35 +0900

    Changed json format for mongoimport.

--- apachelog/import_accesslog.sh
+++ apachelog/import_accesslog.sh
@@ -5,6 +5,6 @@ P=./log/${NAME}
 
 for i in ${P}/access.log*.json ;
 do
-  echo "mongoimport  -d sumibi -c ${NAME} ${i}"
-  time mongoimport  -d sumibi -c ${NAME} ${i}
+  echo "mongoimport  --host black -d sumibi -c ${NAME} ${i}"
+  time mongoimport  --host black -d sumibi -c ${NAME} ${i}
 done
--- apachelog/mongo_operation.js
+++ apachelog/mongo_operation.js
@@ -3,19 +3,19 @@
              This file is a memo file of mongo shell operations.
 
 
- $ mongo --host black sumibi
+$ mongo --host black sumibi
 MongoDB shell version: 2.0.1
 connecting to: black:27017/test
 > use sumibi
 switched to db sumibi
 > show collections
-apachelog
+sumibiorg
 system.indexes
-> db.apachelog.count();
+> db.sumibiorg.count();
 6072350
 
 
-> a = db.apachelog.findOne().timestamp;
+> a = db.sumibiorg.findOne().timestamp;
 ISODate("2011-10-08T21:28:36Z")
 > a.getDay();
 0
@@ -28,13 +28,13 @@ ISODate("2011-10-08T15:00:00Z")
 > b
 ISODate("2011-10-08T15:00:00Z")
 
-> var cursor = db.apachelog.find().limit(2);
+> var cursor = db.sumibiorg.find().limit(2);
 > cursor.forEach(function(x) { print( x.timestamp ); });
 Sun Oct 09 2011 06:28:36 GMT+0900 (JST)
 Sun Oct 09 2011 06:28:40 GMT+0900 (JST)
 
 
-> var cursor = db.apachelog.find().limit(2);
+> var cursor = db.sumibiorg.find().limit(2);
 > cursor.forEach(function(x) {
    d1 = x.timestamp
    d2 = new Date( d1.getFullYear(), d1.getMonth(), d1.getDate(), 0, 0, 0 )
@@ -44,64 +44,86 @@ Sun Oct 09 2011 00:00:00 GMT+0900 (JST)
 Sun Oct 09 2011 00:00:00 GMT+0900 (JST)
 
 
-> var cursor = db.apachelog.find().limit(2);
-> cursor.forEach(function(x) {
-   d1 = x.timestamp
-   d2 = new Date( d1.getFullYear(), d1.getMonth(), d1.getDate(), 0, 0, 0 )
-   print( x._id )
-   db.apachelog.update( { _id: x._id }, { "$set" : { "timestampDaily" : d2 }} );
- });
-
-
+# test for heavy query
+var cursor = db.sumibiorg.find().limit(2);
+cursor.forEach(function(x) {
+    d1 = x.timestamp
+    d2 = new Date( d1.getFullYear(), d1.getMonth(), d1.getDate(), 0, 0, 0 )
+    print( x._id )
+    db.sumibiorg.update( { _id: x._id }, { "$set" : { "timestampDaily" : d2 }} );
+})
 
 #  heavy query
-> var cursor = db.apachelog.find();
-> cursor.forEach(function(x) {
-   d1 = x.timestamp
-   d2 = new Date( d1.getFullYear(), d1.getMonth(), d1.getDate(), 0, 0, 0 )
-   db.apachelog.update( { _id: x._id }, { "$set" : { "timestampDaily" : d2 }} );
- });
+var cursor = db.sumibiorg.find();
+cursor.forEach(function(x) {
+    d1 = x.timestamp
+    d2 = new Date( d1.getFullYear(), d1.getMonth(), d1.getDate(), 0, 0, 0 )
+    db.sumibiorg.update( { _id: x._id }, { "$set" : { "timestampDaily" : d2 }} );
+})
 
-db.apachelog.ensureIndex( { timestampDaily : 1 } )
+db.sumibiorg.ensureIndex( { timestampDaily : 1 } )
 
-a = db.apachelog.distinct( "timestampDaily" )
+a = db.sumibiorg.distinct( "timestampDaily" )
 
 for ( i = 0 ; i < a.length ; i++ ) {
   print ( a[i] )
 }
 
 
-# group by
-g = db.apachelog.group(
+# group by timestampDaily
+g = db.sumibiorg.group(
     {key: { timestampDaily:true },
      cond: {},
      reduce: function(obj,prev) { prev.count += 1; },
      initial: { count: 0 }
-    });
+    })
 
 for ( i = 0 ; i < g.length ; i++ ) {
     d = g[i].timestampDaily
-    print( d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate() + "    " + g[i].count )
+    print( d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate() + "\t" + g[i].count )
+}
+
+
+# group by timestampDaily  and filtering sumibi.cgi
+g = db.sumibiorg.group(
+    {key: { timestampDaily:true },
+     cond: { url : "/cgi-bin/sumibi/testing/sumibi.cgi" },
+     reduce: function(obj,prev) { prev.count += 1; },
+     initial: { count: 0 }
+    })
+
+for ( i = 0 ; i < g.length ; i++ ) {
+    d = g[i].timestampDaily
+    print( d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate() + "\t" + g[i].count )
 }
 
 
 
 
+
 # heavy query
-> var cursor = db.apachelog.find()
-> cursor.forEach(function(x) {
-   lst = x.request.split(/ /)
-   db.apachelog.update( { _id: x._id }, { "$set" : 
-					  { "req" : 
-					    { "method" : lst[0],
-					      "url"    : lst[1] }}} )
- });
+var cursor = db.sumibiorg.find()
+cursor.forEach(function(x) {
+    lst = x.request.split(/ /)
+    db.sumibiorg.update( { _id: x._id }, { "$set" : 
+					   { "req" : 
+					     { "method" : lst[0],
+					       "url"    : lst[1] }}} )
+})
+db.sumibiorg.ensureIndex( { url : 1 } )
 
-> db.apachelog.ensureIndex( { url : 1 } )
+var cursor = db.sumibiorg.find()
+cursor.forEach(function(x) {
+    lst = x.request.split(/ /)
+    db.sumibiorg.update( { _id: x._id }, { "$set" : { "method" : lst[0] }} )
+    db.sumibiorg.update( { _id: x._id }, { "$set" : { "url" :    lst[1] }} )
+})
+db.sumibiorg.ensureIndex( { url : 1 } )
 
-> db.apachelog.distinct( "req.url" );
 
-g = db.apachelog.group(
+db.sumibiorg.distinct( "req.url" );
+
+g = db.sumibiorg.group(
     {key: { req : { url:true }},
      cond: {},
      reduce: function(obj,prev) { prev.count += 1; },
@@ -110,11 +132,11 @@ g = db.apachelog.group(
 
 
 for ( i = 0 ; i < g.length ; i++ ) {
-    db.apachelog.top.insert( { url: g[i].req.url, count: g[i].count } );
+    db.sumibiorg.top.insert( { url: g[i].req.url, count: g[i].count } );
 }
 
 # Top access url as TSV format.
-db.apachelog.top.find().sort( { count: -1 } ).limit( 50 ).forEach( function( x ) {
+db.sumibiorg.top.find().sort( { count: -1 } ).limit( 50 ).forEach( function( x ) {
     print( x.count + "\t" + x.url );
 } )
 
